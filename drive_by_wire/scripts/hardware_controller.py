8d27d60ee62cd0117f21e0dae3f074c05851cc64
alanhesu <ahesu6@gatech.edu>
2018-10-29 23:16:18 -0400

    some pid hardware controller code

--- drive_by_wire/scripts/hardware_controller.py
+++ drive_by_wire/scripts/hardware_controller.py
@@ -1,32 +1,57 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from drive_by_wire.msg import Cart_values
 
-def callback(data):
-    constant = 1
+def callback_keyboard(data):
+    # constant = 1
+    global vel_setp
+    vel_setp = data
+    # linear_vel = data.linear.x
+    # angular_vel = data.angular.z
+    # val.throttle = linear_vel
+    # val.brake = data.linear.z
+    # if linear_vel != 0:
+    #     val.steering_angle = constant * (angular_vel / linear_vel)
+    # else:
+    #     val.steering_angle = 0
+    # rospy.loginfo(val.steering_angle)
 
-    global val
-    linear_vel = data.linear.x
-    angular_vel = data.angular.z
-    val.throttle = linear_vel
-    val.brake = data.linear.z
-    if linear_vel != 0:
-        val.steering_angle = constant * (angular_vel / linear_vel)
-    else:
-        val.steering_angle = 0
-    rospy.loginfo(val.steering_angle)
+def callback_odom(data):
+    global vel_curr
+    vel_curr = data.twist.Twist
+    stamp = data.header.stamp.secs + data.header.stamp.nsecs/1000000000
 
 
 pub = rospy.Publisher('Arduino_commands', Cart_values, queue_size=10)
 rospy.init_node('hardware_controller', anonymous=True)
-rospy.Subscriber("keyboard", Twist, callback)
+rospy.Subscriber("keyboard", Twist, callback_keyboard)
+rospy.Subscriber("odom", Odometry, callback_odom)
 rate = rospy.Rate(10)
 val = Cart_values()
+vel_setp = Twist()
+vel_curr = Twist()
+
+stamp = .001
+prev = 0
+accum = 0
+kp = 1
+kd = 0
+ki = 0
 
 def hardware_controller():
+    global stamp, prev, accum, kp, kd, ki
     while not rospy.is_shutdown():
-        rospy.loginfo(val)
+        error = vel_setp.linear.x - vel_curr.linear.x
+        elapsed = stamp - prev
+        if elapsed == 0:
+            rospy.loginfo("elapsed time is zero!")
+            continue
+        accum += error
+
+        val.throttle = kp*error + kd*error/elapsed + ki*accum
+        prev = stamp
         pub.publish(val)
         rate.sleep()
 
