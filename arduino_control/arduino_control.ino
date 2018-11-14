5e3c76a8a3285fe7ae3cef3dab0fc6201e02e739
alanhesu <alanhesu@gmail.com>
2018-11-13 19:44:24 -0500

    state machine and ekf_continuous.yaml changes

--- arduino_control/arduino_control.ino
+++ arduino_control/arduino_control.ino
@@ -2,16 +2,24 @@
 #include <ros.h>
 #include <drive_by_wire/Cart_values.h>
 #include <std_msgs/Bool.h>
+#include <Cart.h>
+#include <ros/time.h>
 
+enum State {off, start1, start2, start3, rest, pedal1, pedal2, pedal3, brake1};
+State state;
+ros::Time prevTime;
 
 ros::NodeHandle nh;
-float pedalVoltage;
-float steeringVoltage;
-float brakeVoltage;
+
+// Pedal and brake signals
+float pedalVoltage = 0;
+float steeringVoltage = 0;
+float brakeVoltage = 0;
 int pedal = 5;
 int brake = 6;
 int pedalSwitch = 7;
 
+// Wheel encoder
 std_msgs::Bool tick_msg;
 ros::Publisher pub_hall("tick", &tick_msg);
 int reading=0;
@@ -23,26 +31,9 @@ int COUNT=0;
 
 
 void callback(const drive_by_wire::Cart_values& data) {
-//  pedalVoltage = data.throttle;
-//  steeringVoltage = data.steering_angle;
-//  brakeVoltage = data.brake;
-//  if (pedalVoltage != 0) {
-//    digitalWrite(pedalSwitch, HIGH);
-//  } else {
-//    digitalWrite(pedalSwitch, LOW);
-//  }
-//  analogWrite(pedal, pedalVoltage*51);
-//  analogWrite(brake, brakeVoltage*51);
-  if (data.throttle > 0) {
-    digitalWrite(pedalSwitch, HIGH);
-    analogWrite(pedal, data.throttle*51);
-  } else if (data.throttle < 0) {
-    digitalWrite(pedal, 0);
-    digitalWrite(pedalSwitch, LOW);
-    analogWrite(brake, brakeVoltage*51);
-  } else {
-    digitalWrite(pedalSwitch, LOW);
-  }
+  pedalVoltage = data.throttle;
+  steeringVoltage = data.steering_angle;
+  brakeVoltage = data.brake;
 }
 
 ros::Subscriber<drive_by_wire::Cart_values> sub("Arduino_commands", &callback);
@@ -58,10 +49,73 @@ void setup() {
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(pub_hall);
+
+  state = off;  
+  prevTime = nh.now();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // State machine
+  switch(state) {
+    case off:
+      state = start1;
+      prevTime = nh.now();
+      break;
+    case start1:
+      CLOSE_RELAY(EMERGENCY_RELAY);
+      CLOSE_RELAY(ACCEL_ENCODER_ENABLE);
+      CLOSE_RELAY(BRAKE_ENCODER_ENABLE);
+      if (nh.now().toSec() - prevTime.toSec() >= SWITCHING_TIME/1000) {
+        prevTime = nh.now();
+        state = start2;       
+      }
+      break;
+    case start2:
+      CLOSE_RELAY(IGNITION_RELAY);
+      if (nh.now().toSec() - prevTime.toSec() >= 2) {
+        prevTime = nh.now();
+        state = start3;
+      }
+      break;
+    case start3:
+      CLOSE_RELAY(FORWARD_RELAY);
+      state = rest;
+      break;
+     case rest:
+      if (pedalVoltage > 0) {
+        prevTime = nh.now();
+        state = pedal1;
+      } else if (pedalVoltage < 0) {
+        prevTime = nh.now();
+        state = brake1;
+      }
+      break;
+    case pedal1:
+      CLOSE_RELAY(ACCEL_FAILSAFE);
+      if (nh.now().toSec() - prevTime.toSec() >= PEDAL_DELAY) {
+        state = pedal2;
+      }
+      break;
+    case pedal2:
+      if (pedalVoltage < 0) {
+        state = pedal3;
+      } else {
+        analogWrite(ACCEL, pedalVoltage*34);
+        state = pedal2;
+      }
+      break;
+    case pedal3:
+      OPEN_RELAY(ACCEL_FAILSAFE);
+      state = rest;
+      break;
+    case brake1:
+      if (pedalVoltage > 0) {
+        state = rest;
+      } else {
+        analogWrite(BRAKE, -1*pedalVoltage*34);
+        state = brake1;
+      }
+  }
 
   if ((!HallVolt) && (preHall==1)) {
     COUNT+=1;
