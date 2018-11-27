ffcfbdabd0bad8544187bd9fb28c2ea47b70685b
Kevin Choi <kchoi117@gmail.com>
2018-11-27 16:35:42 -0500

    figuring out turning

--- drive_by_wire/scripts/hardware_controller.py
+++ drive_by_wire/scripts/hardware_controller.py
@@ -37,10 +37,10 @@ def callback_odom(data):
     elapsed = stamp - prev
     if elapsed != 0:
         # val.throttle = pid_vel.pid(0, vel_setp.linear.x, stamp)
-        val.throttle = 0.0113*vel_setp**5 + -0.1550*vel_setp**4 + 0.8438*vel_setp**3 + -2.3792*vel_setp**2 + 4.0388*vel_setp + 0.7273
+        val.throttle = 0.0113*vel_setp.linear.x**5 + -0.1550*vel_setp.linear.x**4 + 0.8438*vel_setp.linear.x**3 + -2.3792*vel_setp.linear.x**2 + 4.0388*vel_setp.linear.x + 0.7273
         # if vel_curr.linear.x != 0:
         # val.steering_angle = round(pid_turn.pid(pos_curr.orientation.z, pos_setp.orientation.z, stamp)/vel_curr.linear.x)
-        val.steering_angle = round(pid_turn.pid(pos_curr.orientation.z*math.pi, pos_setp.orientation.z*math.pi, stamp))
+        val.steering_angle = round(pid_turn.pid(pos_curr.orientation.z*math.pi, pos_setp.orientation.z, stamp))
     pub.publish(val)
 
 pub = rospy.Publisher('Arduino_commands', Cart_values, queue_size=10)
