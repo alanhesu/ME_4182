c284abcef0296d903ca81ef6bbdda0e6b726c8cc
czheng45 <37811279+czheng45@users.noreply.github.com>
2018-11-29 15:36:09 -0500

    Updated relays
    Relay number changed for new mega shield.
    
    EMERGENCY_RELAY replaced with STEPPER_RELAY

--- arduino_control/Cart/Cart.h
+++ arduino_control/Cart/Cart.h
@@ -3,20 +3,19 @@
 
 
 /*Control relay pin definitions*/
-#define EMERGENCY_RELAY 29 //Power line which starts up the vehicle. Must be held closed to operate vehicle. If unpowered, opens and stops vehicle.
-#define IGNITION_RELAY 27 //Power line which must be held closed to switch to FWD or REV
-#define	FORWARD_RELAY 23 //If closed, cart drives in Forward mode
-#define REVERSE_RELAY 25 //If closed, cart drives in Reverse mode
+#define STEPPER_RELAY 22 //Power line which starts up the vehicle. Must be held closed to operate vehicle. If unpowered, opens and stops vehicle.
+#define IGNITION_RELAY 24 //Power line which must be held closed to switch to FWD or REV
+#define	FORWARD_RELAY 28 //If closed, cart drives in Forward mode
+#define REVERSE_RELAY 26 //If closed, cart drives in Reverse mode
 //If neither the Forward or Reverse relays are closed, but both the Emergency relay and ignition are closed, the cart is in neutral
-#define ACCEL_FAILSAFE 26 //Switch for acceleration pedal. In order to send acceleration signal without faulting, this must be closed before signaling
-#define BRAKE_FULLSTOP 28 //Switch for brake pedal. Closing this WHILE the brake signal is >3.5V signals a full-stop
-#define ACCEL_ENCODER_ENABLE 24 //Switch for swapping to spoofed accel signal in. These should simply be closed upon autonomous mode start
-#define BRAKE_ENCODER_ENABLE 22 //Switch for swapping to spoofed brake signal in. These should simply be closed upon autonomous mode start
+#define ACCEL_FAILSAFE 27 //Switch for acceleration pedal. In order to send acceleration signal without faulting, this must be closed before signaling
+#define BRAKE_FULLSTOP 29 //Switch for brake pedal. Closing this WHILE the brake signal is >3.5V signals a full-stop
+#define ACCEL_ENCODER_ENABLE 25 //Switch for swapping to spoofed accel signal in. These should simply be closed upon autonomous mode start
+#define BRAKE_ENCODER_ENABLE 23 //Switch for swapping to spoofed brake signal in. These should simply be closed upon autonomous mode start
 #define CLOSE_RELAY(x) (pinMode(x,OUTPUT))
 #define OPEN_RELAY(x) (pinMode(x,INPUT))
 #define SWITCHING_TIME 5
 #define PEDAL_DELAY 0
-#define STEPPER_RELAY 30
 
 /*Accel & Brake signal definitions*/
 #define ACCEL 6
