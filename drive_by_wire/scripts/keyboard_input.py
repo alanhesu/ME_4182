ab35508b989f9ea8828f03e4fea0a9d5b2bddc5e
alanhesu <alanhesu@gmail.com>
2018-12-02 15:15:01 -0500

    manual mode from console

--- drive_by_wire/scripts/keyboard_input.py
+++ drive_by_wire/scripts/keyboard_input.py
@@ -3,48 +3,37 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from drive_by_wire.msg import Cart_values
 # import keyboard
 
-pub = rospy.Publisher('keyboard', Twist, queue_size=10)
+pub = rospy.Publisher('Arduino_commands', Cart_values, queue_size=10)
 rospy.init_node('keyboard_input', anonymous=True)
 rate = rospy.Rate(30) # 10hz
-vel_msg = Twist()
-vel_max_linear = 5
-min_turn_radius = 3
-vel_max_angular = vel_max_linear * min_turn_radius
-vel_linear_inc = 3
-vel_angular_inc = .1
+val = Cart_values()
+val.is_manual = False
 
 def keyboard_input():
-    global vel_msg
+    global val
     while not rospy.is_shutdown():
         text = raw_input()
-        if 'w' in text:
-            if vel_msg.linear.x < vel_max_linear:
-                vel_msg.linear.x += vel_linear_inc
-        if 's' in text:
-            if vel_msg.linear.x > -1*vel_max_linear:
-                vel_msg.linear.x -= vel_linear_inc
-        if 'd' in text:
-            if vel_msg.angular.z > -1*vel_max_angular:
-                vel_msg.angular.z -= vel_angular_inc
-        if 'a' in text:
-            if vel_msg.angular.z < vel_max_angular:
-                vel_msg.angular.z += vel_angular_inc
+        # if 'w' in text:
+        #     if vel_msg.linear.x < vel_max_linear:
+        #         vel_msg.linear.x += vel_linear_inc
+        # if 's' in text:
+        #     if vel_msg.linear.x > -1*vel_max_linear:
+        #         vel_msg.linear.x -= vel_linear_inc
+        # if 'd' in text:
+        #     if vel_msg.angular.z > -1*vel_max_angular:
+        #         vel_msg.angular.z -= vel_angular_inc
+        # if 'a' in text:
+        #     if vel_msg.angular.z < vel_max_angular:
+        #         vel_msg.angular.z += vel_angular_inc
         if 'b' in text:
-            vel_msg.linear.z = 3
-            vel_msg.linear.x = 0
-        if 'n' in text:
-            vel_msg.linear.z = 0
-        pub.publish(vel_msg)
-        rospy.loginfo(vel_msg)
+            val.is_manual = not val.is_manual
+        # if 'n' in text:
+        #     vel_msg.linear.z = 0
+        pub.publish(val)
         rate.sleep()
-
-    vel_msg.linear.x = 0
-    vel_msg.linear.z = 0
-    vel_msg.angular.z = 0
-    pub.publish(vel_msg)
-
 
 if __name__ == '__main__':
     try:
